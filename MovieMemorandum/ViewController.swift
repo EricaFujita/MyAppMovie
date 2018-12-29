e5b51dd3cb2ca4b9606512b392a3e7205a8a54a8
藤田えりか <ericafujita@fujitaerikanomacbook-pro.local>
2018-12-30 00:01:14 +0900

    Initial Commit

---
+++ MovieMemorandum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieMemorandum
+//
+//  Created by 藤田えりか on 2018/12/30.
+//  Copyright © 2018 com.erica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
